bc384b7b42cd599fc7739251fe64043ce3aded6e
komatsuzakiyuma <38421478+komatsuzakiyuma@users.noreply.github.com>
2018-11-12 17:48:51 +0900

    Create default.js

---
+++ is/default.js
@@ -0,0 +1,32 @@
+//確認ダイアログを表示
+
+var result=wnidow.confirm('準備OK');
+
+//結果を表示
+if(result){
+
+document.getElementById('choice').textContent='OKが押されました';
+}else{
+    document.getElementById('choice').textContent='キャンセルが押されました';
+}
+
+document.getElementById('choice').textContent=result;
+
+//答えを用意
+
+var nuber=Math.floor(Math.random()*3);
+var answer=parseInt(wnidow.prompt('数あてゲーム。０～２の数字を入力してください！'));
+
+var message;
+if(answer===nuber){
+    message='あたり!';
+}else if(answer < nuber){
+    message='残念！もっと大きいです';
+}else if(nuber < answer){
+    message = '残念！もっと小さいです';
+
+}
+
+//結果を表示
+document.getElementById('choice').textContent = message
+
