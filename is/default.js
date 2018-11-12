08d378261cfdc75cd75e639be83c825e4c167e8d
komatsuzakiyuma <38421478+komatsuzakiyuma@users.noreply.github.com>
2018-11-12 17:51:47 +0900

    Update default.js

--- is/default.js
+++ is/default.js
@@ -1,17 +1,4 @@
-//確認ダイアログを表示
-
-var result=wnidow.confirm('準備OK');
-
-//結果を表示
-if(result){
-
-document.getElementById('choice').textContent='OKが押されました';
-}else{
-    document.getElementById('choice').textContent='キャンセルが押されました';
-}
-
-document.getElementById('choice').textContent=result;
-
+/
 //答えを用意
 
 var nuber=Math.floor(Math.random()*3);
